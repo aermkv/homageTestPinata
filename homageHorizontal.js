5419db30ba604877ecd2736d229003e769c34423
aermkv <anton.ermkv@gmail.com>
2024-09-12 18:51:32 -0400

    Update homageHorizontal.js

--- homageHorizontal.js
+++ homageHorizontal.js
@@ -178,8 +178,8 @@ const depData = {
 	  },
 	],
   // preferredIPFSGateway:"https://black-indirect-dragon-602.mypinata.cloud"
-  // preferredIPFSGateway:"https://proj.myfilebase.com"
-  preferredIPFSGateway:"https://ipfs.filebase.io/ipfs/"
+  preferredIPFSGateway:"https://proj.myfilebase.com"
+  // preferredIPFSGateway:"https://ipfs.filebase.io/ipfs/"
 }
 
 

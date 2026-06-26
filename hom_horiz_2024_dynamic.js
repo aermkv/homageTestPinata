d0e9737dbcd2e9f84f4e09ce7bfb09e258d55026
aermkv <anton.ermkv@gmail.com>
2026-06-26 15:52:15 -0400

    Update preferred IPFS gateway configuration
    Comment out the old preferred IPFS gateway and add a new one.

--- hom_horiz_2024_dynamic.js
+++ hom_horiz_2024_dynamic.js
@@ -180,8 +180,9 @@ const depData = {
 	  },
 	],
   // preferredIPFSGateway:"https://black-indirect-dragon-602.mypinata.cloud"
-  preferredIPFSGateway:"https://homage2024.myfilebase.com"
+  // preferredIPFSGateway:"https://homage2024.myfilebase.com"
   // preferredIPFSGateway:"https://ipfs.filebase.io/ipfs/"
+  prefferedIPFSGateway:"https://crucial-sapphire-mammal.myfilebase.com/"
 }
 
 
